0fdd530a80dccf139c904cb5a56260386306cb3a
AZ <wira123widodo@gmail.com>
2023-10-12 10:30:37 +0700

    style[AW]: create template input

---
+++ src/website/input.jsx
@@ -0,0 +1,11 @@
+import { Icon } from "@iconify/react";
+import { useState, useEffect } from "react";
+import { useNavigate, Link, useParams } from "react-router-dom";
+import apiurl from "../api/api";
+import axios from "axios";
+
+function pageInput() {
+    
+}
+
+export default pageInput;
