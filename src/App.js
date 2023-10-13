c721f4d79ed963b0709628169870e6723254e20a
AZ <wira123widodo@gmail.com>
2023-10-13 12:47:11 +0700

    style[AW]: create page db

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
 import FormInput from '../src/website/input';
+import Db from '../src/website/db';
 
 function App() {
   return (
     <Router>
       <Routes>
          <Route path="/forminput" element={<FormInput/>} />  
+         <Route path="/db" element={<Db/>} />
       </Routes>
     </Router>
   );
--- src/website/input.jsx
+++ src/website/input.jsx
@@ -1,12 +1,13 @@
 // import { Icon } from "@iconify/react";
 import { useState, useEffect } from "react";
-import LogoAPKB from '../Assets/LOGOAPKB.png';
-import '../Style/inputform.css';
+import LogoAPKB from "../Assets/LOGOAPKB.png";
+import "../Style/inputform.css";
 import { useNavigate, Link, useParams } from "react-router-dom";
 // import apiurl from "../api/api";
 // import axios from "axios";
 
 function FormInputSuratPenugasan() {
+  const navigate = useNavigate();
   return (
     <div>
       <header>
@@ -17,7 +18,7 @@ function FormInputSuratPenugasan() {
           <li className="active">
             <a href="">INPUT</a>
           </li>
-          <li>
+          <li onClick={() => navigate("/db")}>
             <a href="">DB</a>
           </li>
           <li>
@@ -86,6 +87,68 @@ function FormInputSuratPenugasan() {
             <div className="button-tangging">
               <button id="cancel">BATAL</button>
               <button id="continue">Kegiatan Online</button>
+            </div>
+          </div>
+        </div>
+        <div className="body-form">
+          <div className="form-input">
+            <p>Dasar Pelaksanaan Tugas</p>
+            <input type="text" placeholder="Input Dasar Pelaksanaan Tugas" />
+          </div>
+          <div className="form-input">
+            <p>Kota Tujuan Tugas I</p>
+            <input type="text" placeholder="Input Kota Tujuan Tugas I" />
+          </div>
+          <div className="form-input">
+            <p>Kota Tujuan Tugas II</p>
+            <input type="text" placeholder="Input Kota Tujuan Tugas II" />
+          </div>
+          <div className="form-input">
+            <p>Kota Tujuan Tugas III</p>
+            <input type="text" placeholder="Input Kota Tujuan Tugas III" />
+          </div>
+          <div className="form-input">
+            <p>Maksud Perjalanan Dinas</p>
+            <input type="text" placeholder="Input Maksud Perjalanan Dinas" />
+          </div>
+          <div className="form-input">
+            <p>Kantor Tujuan Tugas</p>
+            <input type="text" placeholder="Input Kantor Tujuan Tugas" />
+          </div>
+          <div className="form-input">
+            <p>Transport</p>
+            <input type="text" placeholder="Input Transport" />
+          </div>
+          <div className="form-input">
+            <p>Pegawai</p>
+            <input type="text" placeholder="Input Pegawai" />
+          </div>
+          <div className="form-input">
+            <p>Pejabat Penanda Tangan ST & SPD</p>
+            <input
+              type="text"
+              placeholder="Input Pejabat Penanda Tangan ST & SPD"
+            />
+          </div>
+          <div className="form-input">
+            <p>Kantor Tujuan Tugas Spesifik (Spd Dalam Kota Saja)</p>
+            <textarea
+              name=""
+              id=""
+              rows="5"
+              placeholder="Input Kantor Tujuan Tugas Spesifik"
+              style={{ resize: 'none' }}
+            ></textarea>
+          </div>
+          <div className="form-input">
+            <p>Kota Asal Tujuan</p>
+            <input type="text" placeholder="Input Kota Asal Tujuan" />
+          </div>
+          <div className="form-input">
+            <div className="button-form">
+              <button id="save">Simpan</button>
+              <button id="clear">Clear Form</button>
+              <button id="add">Tambah Pegawai</button>
             </div>
           </div>
         </div>
